33eefcec3c9d9bea204ed4e1b8bc81cb235513ad
nleung77 <nleung77@gmail.com>
2023-04-11 14:41:51 -0400

    update file structure

--- routes/launches.js
+++ routes/launches.js
@@ -3,16 +3,16 @@ import * as launches from "../controllers/launches.js";
 
 const router = Router()
 
-router.get("/launches", launches.getLaunches);
-router.get("/launches/:id", launches.getLaunch);
-router.post("/launches", launches.createLaunch);
-router.put("/launches/:id", launches.updateLaunch);
-router.delete("/launches/:id", launches.deleteLaunch);
+router.get("/", launches.getLaunches);
+router.get("/:id", launches.getLaunch);
+router.post("/", launches.createLaunch);
+router.put("/:id", launches.updateLaunch);
+router.delete("/:id", launches.deleteLaunch);
 
-router.get("/launches/name/:name", launches.getLaunchesByName);
-router.get("/launches/launchpad/:launchpad", launches.getLaunchesByLaunchpad);
-router.get("/launches/upcoming", launches.getUpcomingLaunches);
-router.get("/launches/failed", launches.getFailedLaunches);
-router.get("/launches/flightnumber/:flightNumber", launches.getLaunchesByFlightNumber);
+router.get("/name/:name", launches.getLaunchesByName);
+router.get("/launchpad/:launchpad", launches.getLaunchesByLaunchpad);
+router.get("/upcoming", launches.getUpcomingLaunches);
+router.get("/failed", launches.getFailedLaunches);
+router.get("/flightnumber/:flightNumber", launches.getLaunchesByFlightNumber);
 
 export default router;
